4cc531d8e858f287caa4d0dc163bb7aecb79c844
RoryDME5 <RoryDME5@users.noreply.github.com>
2018-04-13 12:47:10 +0100

    Jquery testing

---
+++ js/application.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function ($){
+    //jQuery('.js-tester').load("js-tester.txt");
+    //$('#containing-element').html("<div class='ml-2'><a href='https://twitter.com/centrecom_ie' class='fa navbar-social fa-twitter'></a><a href='https://www.linkedin.com/company/centrecom/' class='fa navbar-social fa-linkedin'></a></div>");
+    //$('#containing-element').load("js-tester.txt");
+});
